d138d4ba24684618d797d9c4c29240d0ddca5cfc
Arnault Chazareix <arnaultc@sicara.com>
2020-11-30 00:42:23 +0100

    add first test

---
+++ tests/test_streamlit_frontend.py
@@ -0,0 +1,9 @@
+from unittest.mock import patch
+from tqdm import tqdm
+from stqdm.stqdm import stqdm
+
+@patch("streamlit.empty")
+def test_write_tqdm_description(empty_mock):
+    stqdmed_iterator = stqdm(range(2))
+    for _ in stqdmed_iterator:
+        empty_mock.return_value.write.assert_called_with(tqdm.format_meter(**{**stqdmed_iterator.format_dict, "ncols": 0}))
