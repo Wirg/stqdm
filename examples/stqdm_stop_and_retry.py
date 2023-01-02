51535f2842f72c6853efe923ebdd9020f6cc927d
Arnault Chazareix <arnaultc@sicara.com>
2023-01-02 19:08:47 +0100

    add more examples

---
+++ examples/stqdm_stop_and_retry.py
@@ -0,0 +1,27 @@
+"""
+Based on https://github.com/Wirg/stqdm/issues/64#issuecomment-1250906739
+There is some issue with stqdm on Windows.
+Stopping this from the web frontend will create some locking issues.
+You can solve this with using :
+
+```python
+from threading import RLock
+from stqdm import stqdm
+
+stqdm.set_lock(RLock())
+```
+
+See : https://github.com/Wirg/stqdm/issues/64#issuecomment-1367536561
+"""
+from time import sleep
+
+import streamlit as st
+
+submit = st.button("Click me")
+text_widget = st.empty()
+progress_widget = st.empty()
+if submit:
+    for i in range(50):
+        sleep(0.5)
+        text_widget.write(i)
+        progress_widget.progress(i / 50)
---
+++ examples/stqdm_with_caching_and_speed.py
@@ -0,0 +1,33 @@
+"""
+How to cache response.
+This is a base streamlit util not related to stqdm.
+See : https://docs.streamlit.io/library/advanced-features/caching
+
+From : https://discuss.streamlit.io/t/stqdm-a-tqdm-like-progress-bar-for-streamlit/10097/20?u=wirg
+"""
+
+
+from time import sleep
+from typing import List
+
+import streamlit as st
+
+from stqdm import stqdm
+
+
+def process_for_index(index: int) -> int:
+    sleep(0.05)
+    return 2 * index + 1
+
+
+@st.cache  # Use streamlit to cache the results of this function for i
+def process_for_multiple_indexes(indexes: List[int]) -> List[int]:
+    return [process_for_index(index) for index in indexes]
+
+
+BATCH_SIZE = 500
+
+for batch_start in stqdm(range(0, 5000, BATCH_SIZE)):
+    batch_results = process_for_multiple_indexes(list(range(batch_start, batch_start + BATCH_SIZE)))
+    for result in batch_results:
+        st.write(result)
