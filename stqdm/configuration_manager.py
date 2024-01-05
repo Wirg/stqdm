f10e21766dca70c40124e45cf4f290c0dcf75517
Arnault Chazareix <arnaultc@sicara.com>
2024-01-05 15:08:22 -0500

    chore(configuration_manager): separate default config from the stack

--- stqdm/configuration_manager.py
+++ stqdm/configuration_manager.py
@@ -1,26 +1,22 @@
 from abc import ABCMeta
 from contextlib import contextmanager
-from typing import Any, Generic, Iterator, List, Mapping, Optional, TypeVar, cast
-
-
-class ScopeError(RuntimeError):
-    pass
-
+from typing import Any, Generic, Iterator, List, Mapping, TypeVar, cast
 
 ScopeConfig = TypeVar("ScopeConfig", bound=Mapping[str, Any])
 
 
 class ScopeManager(Generic[ScopeConfig], metaclass=ABCMeta):  # pylint: disable=invalid-name,inconsistent-mro
-    def __init__(self, stack: Optional[List[ScopeConfig]] = None) -> None:
-        if stack is None:
-            stack = []
-        self._scope_stack: List[ScopeConfig] = stack
+    def __init__(self, default_config: ScopeConfig) -> None:
+        if not isinstance(default_config, Mapping):
+            raise TypeError("Default config is not an instance of Mapping.")
+        self.default_config: ScopeConfig = default_config
+        self._scope_stack: List[ScopeConfig] = []
 
-    def set_default_kwargs(self, scope_config: ScopeConfig) -> None:
-        if not self._scope_stack:
-            self._scope_stack.append(scope_config)
-        else:
-            self._scope_stack[0] = scope_config
+    def set_default_config(self, default_config: ScopeConfig) -> None:
+        self.default_config = default_config
+
+    def get_default_config(self) -> ScopeConfig:
+        return self.default_config
 
     @contextmanager
     def scope(self, scope_config: ScopeConfig) -> Iterator[ScopeConfig]:
@@ -30,11 +26,11 @@ class ScopeManager(Generic[ScopeConfig], metaclass=ABCMeta):  # pylint: disable=
         finally:
             self._scope_stack.pop()
 
-    def get_current_defaults(self) -> ScopeConfig:
+    def get_current_scope_config(self) -> ScopeConfig:
         if self._scope_stack:
             return self._scope_stack[-1]
-        raise ScopeError("No default scope set. You may have messed up with the scope stack. Please report this issue.")
+        return {}
 
     def use_current_default_if_config_not_provided(self, config: ScopeConfig) -> ScopeConfig:
         # This typing is probably wrong, but in our case, we will be using a TypedDict and it should be ok
-        return cast(ScopeConfig, {**self.get_current_defaults(), **config})
+        return cast(ScopeConfig, {**self.get_default_config(), **self.get_current_scope_config(), **config})
--- tests/test_configuration_manager.py
+++ tests/test_configuration_manager.py
@@ -1,51 +1,48 @@
-import pytest
-
-from stqdm.configuration_manager import ScopeError, ScopeManager
+from stqdm.configuration_manager import ScopeManager
 
 
-def test_scope_manager__get_current_defaults():
-    scope_manager = ScopeManager([{}])
-    assert scope_manager.get_current_defaults() == {}
-    scope_manager = ScopeManager([{"foo": "bar"}])
-    assert scope_manager.get_current_defaults() == {"foo": "bar"}
+def test_scope_manager__get_default_config():
+    scope_manager = ScopeManager({})
+    assert scope_manager.get_default_config() == {}
+    scope_manager = ScopeManager({"foo": "bar"})
+    assert scope_manager.get_default_config() == {"foo": "bar"}
 
 
-def test_scope_manager__set_default_kwargs():
-    scope_manager = ScopeManager([{}])
-    assert scope_manager.get_current_defaults() == {}
-    scope_manager.set_default_kwargs({"foo": "bar"})
-    assert scope_manager.get_current_defaults() == {"foo": "bar"}
-    scope_manager.set_default_kwargs({"foo": "baz", "bar": "foo"})
-    assert scope_manager.get_current_defaults() == {"foo": "baz", "bar": "foo"}
+def test_scope_manager__set_default_config():
+    scope_manager = ScopeManager({})
+    assert scope_manager.get_default_config() == {}
+    scope_manager.set_default_config({"foo": "bar"})
+    assert scope_manager.get_default_config() == {"foo": "bar"}
+    scope_manager.set_default_config({"foo": "baz", "bar": "foo"})
+    assert scope_manager.get_default_config() == {"foo": "baz", "bar": "foo"}
+
+
+def test_scope_manager__scope():
+    scope_manager = ScopeManager({})
+    assert scope_manager.get_current_scope_config() == {}
+    with scope_manager.scope({"foo": "bar"}):
+        assert scope_manager.get_current_scope_config() == {"foo": "bar"}
+        with scope_manager.scope({"foo": "baz"}):
+            assert scope_manager.get_current_scope_config() == {"foo": "baz"}
+        assert scope_manager.get_current_scope_config() == {"foo": "bar"}
+    assert scope_manager.get_current_scope_config() == {}
 
 
 def test_scope_manager__use_current_default_if_config_not_provided():
-    scope_manager = ScopeManager([{"foo": "bar"}])
+    scope_manager = ScopeManager({"foo": "bar"})
     assert scope_manager.use_current_default_if_config_not_provided({}) == {"foo": "bar"}
     assert scope_manager.use_current_default_if_config_not_provided({"foo": "baz"}) == {"foo": "baz"}
     assert scope_manager.use_current_default_if_config_not_provided({"bar": "foo"}) == {"foo": "bar", "bar": "foo"}
 
 
-def test_scope_manager__scope():
-    scope_manager = ScopeManager([{}])
-    assert scope_manager.get_current_defaults() == {}
-    with scope_manager.scope({"foo": "bar"}):
-        assert scope_manager.get_current_defaults() == {"foo": "bar"}
-        with scope_manager.scope({"foo": "baz"}):
-            assert scope_manager.get_current_defaults() == {"foo": "baz"}
-        assert scope_manager.get_current_defaults() == {"foo": "bar"}
-    assert scope_manager.get_current_defaults() == {}
-
-
-def test_configuration_manager__raise_for_access_to_non_existing_default():
-    scope_manager = ScopeManager()
-    with pytest.raises(ScopeError):
-        scope_manager.get_current_defaults()
-    with pytest.raises(ScopeError):
-        scope_manager.use_current_default_if_config_not_provided({})
-
-
-def test_configuration_manager__can_set_default_when_empty_stack():
-    scope_manager = ScopeManager()
-    scope_manager.set_default_kwargs({"foo": "bar"})
-    assert scope_manager.get_current_defaults() == {"foo": "bar"}
+def test_scope_manager__use_current_default_if_config_not_provided_with_scopes():
+    scope_manager = ScopeManager({"foo": "bar"})
+    assert scope_manager.use_current_default_if_config_not_provided({}) == {"foo": "bar"}
+    with scope_manager.scope({"bar": "foo"}):
+        assert scope_manager.use_current_default_if_config_not_provided({}) == {"foo": "bar", "bar": "foo"}
+        assert scope_manager.use_current_default_if_config_not_provided({"bar": "hello"}) == {"foo": "bar", "bar": "hello"}
+        with scope_manager.scope({"bar": "baz"}):
+            assert scope_manager.use_current_default_if_config_not_provided({}) == {"foo": "bar", "bar": "baz"}
+            assert scope_manager.use_current_default_if_config_not_provided({"bar": "hello"}) == {"foo": "bar", "bar": "hello"}
+        assert scope_manager.use_current_default_if_config_not_provided({}) == {"foo": "bar", "bar": "foo"}
+    assert scope_manager.use_current_default_if_config_not_provided({}) == {"foo": "bar"}
