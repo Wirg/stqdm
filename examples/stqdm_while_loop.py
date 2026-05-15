88dfa90c1f77d35576112f93737219ddf70abcd5
Arnault Chazareix <arnaultc@sicara.com>
2026-05-15 19:11:01 +0200

    ci: fix lint and commitlint jobs

--- examples/stqdm_while_loop.py
+++ examples/stqdm_while_loop.py
@@ -6,10 +6,10 @@ LENGTH = 50
 INCREMENT = 1
 
 progress = stqdm(total=LENGTH)
-INDEX = 0
+index = 0
 while True:
-    INDEX += INCREMENT
-    if INDEX == LENGTH:
+    index += INCREMENT
+    if index == LENGTH:
         break
     progress.update(INCREMENT)
     sleep(0.5)
